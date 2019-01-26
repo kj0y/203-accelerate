b480a783d91c209c495c01e4c9990e5cafd232da
Kandis Flick <kandisflick@gmail.com>
2019-01-25 18:55:50 -0800

    update about page and styles

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -31,89 +31,93 @@ get_header(); ?>
 		$service_4_detail = get_field('service_4_detail');
 		$image_4 = get_field('image_4'); ?>	
 
-	<div class="header-image">
-		<?php if($header_image) {
-			echo wp_get_attachment_image( $header_image, $size );
-		} ?>
-	</div><!-- main-content -->
+	<div class="header-container">
+		<div class="header-image">
+			<?php if($header_image) {
+				echo wp_get_attachment_image( $header_image, $size );
+			} ?>
+		</div>
+		<div class="centered">
+			<?php if($header_text) {
+				echo $header_text;
+			} ?>
+		</div>
+	</div>
 
 	<div id="about" class="site-content">
+		<article class="services">
+			<h4 class="intro-header"><?php echo $intro_header; ?></h4>
+			<p class="intro"><?php echo $intro; ?></p>
 
-	<article class="services">
-		<h4 class="intro-header"><?php echo $intro_header; ?></h4>
-		<p class="intro"><?php echo $intro; ?></p>
-
-		<div class="services-offered">
-			<div class="service-item">
-				<div class="left-image">
-					<?php if($image_1) {
-						echo wp_get_attachment_image( $image_1 );
-					} ?>
+			<div class="services-offered">
+				<div class="service-item">
+					<div class="left-image">
+						<?php if($image_1) {
+							echo wp_get_attachment_image( $image_1 );
+						} ?>
+					</div>
+					<div class="right-text">
+					<h2 class="service-title">
+						<?php if($service_1) {
+							echo $service_1;
+						} ?></h2><br>
+						<?php if($service_1_detail) {
+							echo $service_1_detail;
+						} ?>
+					</div>
 				</div>
-				<div class="right-text">
-				<h2 class="service-title">
-					<?php if($service_1) {
-						echo $service_1;
-					} ?></h2><br>
-					<?php if($service_1_detail) {
-						echo $service_1_detail;
-					} ?>
+				<div class="service-item">
+					<div class="left-text">
+						<h2 class="service-title"><?php if($service_2) {
+							echo $service_2;
+						} ?></h2><br>
+						<?php if($service_2_detail) {
+							echo $service_2_detail;
+						} ?>
+					</div>
+					<div class="right-image">
+						<?php if($image_2) {
+							echo wp_get_attachment_image( $image_2 );
+						} ?>
+					</div>
 				</div>
-			</div>
-			<div class="service-item">
-				<div class="left-text">
-					<h2 class="service-title"><?php if($service_2) {
-						echo $service_2;
-					} ?></h2><br>
-					<?php if($service_2_detail) {
-						echo $service_2_detail;
-					} ?>
+				<div class="service-item">
+					<div class="left-image">
+						<?php if($image_3) {
+						echo wp_get_attachment_image( $image_3 );
+						} ?>
+					</div>
+					<div class="right-text">
+						<h2 class="service-title"><?php if($service_3) {
+							echo $service_3;
+						} ?></h2><br>
+						<?php if($service_3_detail) {
+							echo $service_3_detail;
+						} ?>
+					</div>
 				</div>
-				<div class="right-image">
-					<?php if($image_2) {
-						echo wp_get_attachment_image( $image_2 );
-					} ?>
-				</div>
-			</div>
-			<div class="service-item">
-				<div class="left-image">
-					<?php if($image_3) {
-					echo wp_get_attachment_image( $image_3 );
-					} ?>
-				</div>
-				<div class="right-text">
-					<h2 class="service-title"><?php if($service_3) {
-						echo $service_3;
-					} ?></h2><br>
-					<?php if($service_3_detail) {
-						echo $service_3_detail;
-					} ?>
-				</div>
-			</div>
-			<div id="bottom-div" class="service-item">
-				<div class="left-text">
-					<h2 class="service-title"><?php if($service_4) {
-						echo $service_4;
-					} ?></h2><br>
-					<?php if($service_4_detail) {
-						echo $service_4_detail;
-					} ?>
-				</div>
-				<div class="right-image">
-					<?php if($image_4) {
-						echo wp_get_attachment_image( $image_4 );
-					} ?>
-				<?php endwhile; // end of the loop. ?>
+				<div id="bottom-div" class="service-item">
+					<div class="left-text">
+						<h2 class="service-title"><?php if($service_4) {
+							echo $service_4;
+						} ?></h2><br>
+						<?php if($service_4_detail) {
+							echo $service_4_detail;
+						} ?>
+					</div>
+					<div class="right-image">
+						<?php if($image_4) {
+							echo wp_get_attachment_image( $image_4 );
+						} ?>
+					<?php endwhile; // end of the loop. ?>
+					</div>
 				</div>
 			</div>
-		</div>
-	</article>
-</div><!-- main-content -->
-
-</div><!-- #primary -->
+		</article>
+	</div><!-- main-content -->
 
 <nav id="navigation" class="container">
 	<div class="services-center"><h1 class="closing-line">Interested in working with us?</h1><a class="button" href="<?php echo site_url('/contact/') ?>">Contact Us</a></div>
 </nav>
-
+				
 <?php get_footer(); ?>
